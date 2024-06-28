25003ae8b9a7335376fd53c20759be6c5aaae494
Kyren223 <ulmliad223@gmail.com>
2024-06-28 23:13:42 +0300

    Modified nvim config, added comment/uncomment keybinding plugin

---
+++ .config/nvim/lua/plugins/comment.lua
@@ -0,0 +1,17 @@
+return {
+    'numToStr/Comment.nvim',
+    opts = {
+        padding = true,
+        sticky = true, -- keep cursor at it's position
+        ignore = nil,
+
+        toggler = { line = 'gcc', block = 'gbc' },
+        opleader = { line = 'gc', block = 'gb' },
+        extra = { above = 'gcO', below = 'gco', eol = 'gcA' },
+
+        mappings = {
+            basic = true, -- `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
+            extra = false, -- `gco`, `gcO`, `gcA`
+        },
+    }}
+
