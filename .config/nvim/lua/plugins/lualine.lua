f0c4646bf588c95e969bd7e62ca22fc39611110b
Kyren223 <ulmliad223@gmail.com>
2024-06-30 22:02:20 +0300

    Added auto-session to nvim and changed lualine's theme from dracula to tokyonight

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,10 +1,12 @@
 return {
     'nvim-lualine/lualine.nvim',
     config = function()
+        -- TODO: Configure this to make the status line fully customizable
         require('lualine').setup({
             options = {
-                theme = 'dracula',
+                theme = 'tokyonight',
             },
+            sections = { lualine_c = { require('auto-session.lib').current_session_name } },
         })
     end,
 }
