b95b6b1c4f996a74d1dc4f92c5092c2aeea9b9ac
Kyren223 <ulmliad223@gmail.com>
2024-07-02 00:57:54 +0300

    Added a bunch of more color schemes to neovim

--- .config/nvim/lua/plugins/colorschemes.lua
+++ .config/nvim/lua/plugins/colorschemes.lua
@@ -1,6 +1,9 @@
 return {
-    { 'catppuccin/nvim', lazy = false, priority = 1000 },
-    { 'folke/tokyonight.nvim', lazy = false, priority = 1000 },
+    { 'catppuccin/nvim',             lazy = false, priority = 1000 },
+    { 'folke/tokyonight.nvim',       lazy = false, priority = 1000 },
     { 'marko-cerovac/material.nvim', lazy = false, priority = 1000 },
-    { 'rebelot/kanagawa.nvim', lazy = false, priority = 1000 },
+    { 'rebelot/kanagawa.nvim',       lazy = false, priority = 1000 },
+    { 'projekt0n/github-nvim-theme', lazy = false, priority = 1000 },
+    { 'EdenEast/nightfox.nvim',      lazy = false, priority = 1000 },
+    { 'rose-pine/neovim',            lazy = false, priority = 1000 },
 }
