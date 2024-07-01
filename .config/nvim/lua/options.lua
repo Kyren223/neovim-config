0b6d242b1c873578096e0d55fba3adfaa959ea47
Kyren223 <ulmliad223@gmail.com>
2024-07-01 16:52:17 +0300

    Updated nvim options to avoid warnings in :checkhealth

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -31,3 +31,9 @@ vim.opt.smartcase = true
 -- Misc
 vim.opt.scrolloff = 8
 vim.opt.undofile = true
+
+-- Disable warnings for missing language providers
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_python3_provider = 0
