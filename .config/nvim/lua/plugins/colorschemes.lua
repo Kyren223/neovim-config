7ea2b61ec162a74794e08419b3848162cc6da12a
Kyren223 <ulmliad223@gmail.com>
2024-07-03 14:48:15 +0300

    Neovim: added more colorschemes, switched default theme to Dracula (IntelliJ's)

--- .config/nvim/lua/plugins/colorschemes.lua
+++ .config/nvim/lua/plugins/colorschemes.lua
@@ -1,9 +1,31 @@
 return {
-    { 'catppuccin/nvim',             lazy = false, priority = 1000 },
-    { 'folke/tokyonight.nvim',       lazy = false, priority = 1000 },
-    { 'marko-cerovac/material.nvim', lazy = false, priority = 1000 },
-    { 'rebelot/kanagawa.nvim',       lazy = false, priority = 1000 },
-    { 'projekt0n/github-nvim-theme', lazy = false, priority = 1000 },
-    { 'EdenEast/nightfox.nvim',      lazy = false, priority = 1000 },
-    { 'rose-pine/neovim',            lazy = false, priority = 1000 },
+    { 'rktjmp/lush.nvim',                   lazy = false, priority = 1001 },
+    { 'catppuccin/nvim',                    lazy = false, priority = 1000 },
+    { 'folke/tokyonight.nvim',              lazy = false, priority = 1000 },
+    { 'marko-cerovac/material.nvim',        lazy = false, priority = 1000 },
+    { 'rebelot/kanagawa.nvim',              lazy = false, priority = 1000 },
+    { 'projekt0n/github-nvim-theme',        lazy = false, priority = 1000 },
+    { 'EdenEast/nightfox.nvim',             lazy = false, priority = 1000 },
+    { 'rose-pine/neovim',                   lazy = false, priority = 1000 },
+    { 'rafi/awesome-vim-colorschemes',      lazy = false, priority = 1000 },
+    { 'datsfilipe/min-theme.nvim',          lazy = false, priority = 1000 },
+    { 'nechaev/darkstorm.nvim',             lazy = false, priority = 1000 },
+    { 'briones-gabriel/darcula-solid.nvim', lazy = false, priority = 1000 },
+    {
+        'okaihe/okai',
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require('okai').setup()
+        end,
+    },
+    {
+        'uloco/bluloco.nvim',
+        lazy = false,
+        priority = 1000,
+        dependencies = { 'rktjmp/lush.nvim' },
+    },
+    { 'doums/darcula', lazy = false, priority = 1000 },
+    { 'pineapplegiant/spaceduck', lazy = false, priority = 1000 },
+    
 }
