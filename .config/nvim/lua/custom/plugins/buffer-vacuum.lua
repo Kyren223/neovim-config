1988046db3bee640f849806138bee5200495e979
Kyren223 <ulmliad223@gmail.com>
2024-07-23 20:55:38 +0300

    chore(neovim): disabled buffer-vacuum until I or the maintainer fixes it

--- .config/nvim/lua/custom/plugins/buffer-vacuum.lua
+++ .config/nvim/lua/custom/plugins/buffer-vacuum.lua
@@ -1,9 +1,12 @@
 return {
     -- FIX: Only cleans 1 buffer at a time
     -- which means if u have more than max buffers, it'll never go below MAX buffers
-    'ChuufMaster/buffer-vacuum',
-    event = 'VeryLazy',
-    opts = {
-        max_buffers = 6,
-    },
+
+    -- 'Kyren223/buffer-vacuum',
+    -- event = 'VeryLazy',
+    -- opts = {
+    --     max_buffers = 6,
+    --     enable_messages = true,
+    --     debug = true,
+    -- },
 }
