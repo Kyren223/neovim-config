86e8c89eb38604bb5e089d9a1ba8b1e4def762e2
Kyren223 <ulmliad223@gmail.com>
2024-07-23 14:06:16 +0300

    fix(neovim): lsp now uses BufReadPre and BufNewFile for lazy loading

--- .config/nvim/lua/custom/plugins/lsp.lua
+++ .config/nvim/lua/custom/plugins/lsp.lua
@@ -2,8 +2,7 @@ return {
     -- NOTE: Make sure the order is as follows:
     -- Mason loads -> Mason lspconfig loads -> Lspconfig loads
     'neovim/nvim-lspconfig',
-    -- event = { 'BufReadPre', 'BufNewFile' },
-    event = "VeryLazy",
+    event = { 'BufReadPre', 'BufNewFile' },
     dependencies = {
         'hrsh7th/cmp-nvim-lsp',
         { 'antosha417/nvim-lsp-file-operations', conifg = true },
