b13bf2adf6a97268c6d2ee459165088b90ba781f
Kyren223 <ulmliad223@gmail.com>
2024-07-30 23:22:47 +0300

    feat(neovim): disabled nvim-autopair only in lua strings

--- .config/nvim/lua/custom/plugins/nvim-autopairs.lua
+++ .config/nvim/lua/custom/plugins/nvim-autopairs.lua
@@ -1,5 +1,11 @@
 return {
     'windwp/nvim-autopairs',
     event = 'InsertEnter',
-    config = true,
+    opts = {
+        check_ts = true,
+        ts_config = {
+            lua = { 'string' }, -- don't add pairs inside lua strings
+            javascript = { 'template_string' }, -- FIX: remove me
+        },
+    },
 }
