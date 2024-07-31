1e4988c4229291393e3985d35f3dda8d58f8578e
Kyren223 <ulmliad223@gmail.com>
2024-07-31 14:30:46 +0300

    chore(neovim): all plugins are now up to date

--- .config/nvim/lua/custom/plugins/ccc.lua
+++ .config/nvim/lua/custom/plugins/ccc.lua
@@ -1,6 +1,10 @@
 return {
     'uga-rosa/ccc.nvim',
-    opts = { highlighter = { auto_enable = true } },
+    opts = {
+        highlighter = { auto_enable = true },
+        -- highlight_mode = 'virtual',
+        -- virtual_pos  = 'inline-right',
+    },
     event = 'VeryLazy',
     keys = {
         { '<leader>cp', '<cmd>CccPick<cr>', desc = '[C]olor [P]icker' },
