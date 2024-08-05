034940a37f2f7b0b2f1ce1b7a005bb3d786eed07
Kyren223 <ulmliad223@gmail.com>
2024-08-05 22:36:40 +0300

    feat(nvim): added hardtime

---
+++ .config/nvim/lua/custom/plugins/hardtime.lua
@@ -0,0 +1,12 @@
+return {
+    'm4xshen/hardtime.nvim',
+    dependencies = { 'MunifTanjim/nui.nvim', 'nvim-lua/plenary.nvim' },
+    event = { 'BufReadPre', 'BufNewFile' },
+    keys = {
+        { '<leader>ht', '<<cmd>Hardtime toggle<cr>', desc = '[H]ardtime [T]oggle' },
+        { '<leader>hT', '<<cmd>Hardtime report<cr>', desc = '[H]ardtime Report' },
+    },
+    opts = {
+        disabled_filetypes = { 'qf', 'lazy', 'mason', 'oil', 'neo-tree' },
+    },
+}
