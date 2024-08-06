d1f0f7c8c754af870ffc49db063813e41ee6dc70
Kyren223 <ulmliad223@gmail.com>
2024-08-06 21:51:17 +0300

    remove(nvim): lsp-lines in favor of tiny-inline-diagnostics

--- .config/nvim/lua/custom/plugins/lsp-lines.lua
+++ .config/nvim/lua/custom/plugins/lsp-lines.lua
@@ -1,11 +1,14 @@
 return {
-    -- NOTE: [Good alternative](https://github.com/rachartier/tiny-inline-diagnostic.nvim)
-    -- although, this plugin is amazing, so I wouldn't switch it most likely
-    'https://git.sr.ht/~whynothugo/lsp_lines.nvim',
-    event = 'VeryLazy',
-    config = function()
-        require('lsp_lines').setup()
-        vim.diagnostic.config({ virtual_lines = false })
-        vim.keymap.set('n', '<Leader>dt', require('lsp_lines').toggle, { desc = '[D]iagnostics [T]oggle inline' })
-    end,
+    -- -- NOTE: [Good alternative](https://github.com/rachartier/tiny-inline-diagnostic.nvim)
+    -- -- although, this plugin is amazing, so I wouldn't switch it most likely
+    -- Welp I did end up switching from it to tiny-inline-diagnostics
+    -- This plugin just has too much info and it creates a ton of ghost lines
+    -- would rather just have the diagnostic of whatever I am on + using trouble for project/file lvl if I want a list
+    -- 'https://git.sr.ht/~whynothugo/lsp_lines.nvim',
+    -- event = 'VeryLazy',
+    -- config = function()
+    --     require('lsp_lines').setup()
+    --     vim.diagnostic.config({ virtual_lines = false })
+    --     vim.keymap.set('n', '<Leader>dt', require('lsp_lines').toggle, { desc = '[D]iagnostics [T]oggle inline' })
+    -- end,
 }
