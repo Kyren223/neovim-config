efab002c3c28d1009e5c13eb56474515983eb178
Kyren223 <ulmliad223@gmail.com>
2024-08-06 12:29:41 +0300

    fix(nvim): nvim notify now filters some unwanted messages

--- .config/nvim/lua/custom/plugins/nvim-notify.lua
+++ .config/nvim/lua/custom/plugins/nvim-notify.lua
@@ -11,7 +11,18 @@ return {
         },
     },
     opts = function()
-        vim.notify = require('notify')
+        -- Filter messages
+        local filtered_messages = { 'No information available', 'No code actions available' }
+        ---@diagnostic disable-next-line: duplicate-set-field
+        vim.notify = function(message, level, opts)
+            opts = opts or {}
+            for _, msg in ipairs(filtered_messages) do
+                if message == msg then
+                    return
+                end
+            end
+            return require('notify')(message, level, opts)
+        end
         return {
             fps = 60,
             render = 'default',
