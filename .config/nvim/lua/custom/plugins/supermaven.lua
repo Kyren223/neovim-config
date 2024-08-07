b12fc059a7b8a21cc75b69c26bdf52ceb531789b
Kyren223 <ulmliad223@gmail.com>
2024-08-08 01:23:40 +0300

    feat(nvim): added supermaven (AI autocompletion)

---
+++ .config/nvim/lua/custom/plugins/supermaven.lua
@@ -0,0 +1,40 @@
+return {
+    'supermaven-inc/supermaven-nvim',
+    event = 'VeryLazy',
+    keys = {
+        {
+            '<leader>sm',
+            function()
+                local api = require('supermaven-nvim.api')
+                if api.is_running() then
+                    Notify('Enabled Supermaven', 'Supermaven')
+                    api.stop()
+                else
+                    Notify('Disabled Supermaven', 'Supermaven')
+                    api.start()
+                end
+            end,
+            desc = '[S]upermaven [T]oggle',
+        },
+    },
+    opts = {
+        keymaps = {
+            accept_suggestion = '<C-y>',
+            clear_suggestion = '<C-]>',
+            accept_word = nil,
+        },
+        ignore_filetypes = {
+            markdown = true,
+            ['NeogitStatus'] = true,
+            ['NeogitPopup'] = true,
+            ['NeogitCommitMessage'] = true,
+        },
+        color = {
+            suggestion_color = '#ffffff',
+            cterm = 244,
+        },
+        log_level = 'info', -- set to "off" to disable logging completely
+        disable_inline_completion = false, -- disables inline completion for use with cmp
+        disable_keymaps = false,
+    },
+}
